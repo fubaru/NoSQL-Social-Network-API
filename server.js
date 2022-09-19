5f11ab07bffaf2a463e04f3774b87de57f3e94e6
fuaus <kotingfunk@gmail.com>
2022-09-19 15:52:47 -0400

    complete server.js files

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const app = express();
 const PORT = 3001;
 
 const db = require('./config/connection');
 const routes = require('./routes');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.use(routes);
+
+db.once('open', () => {
+    app.listen(PORT, () => {
+        console.log(`API server for Social NetWork API running on port ${PORT}!`)
+    });
+});
